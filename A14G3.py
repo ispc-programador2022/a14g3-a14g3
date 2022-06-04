e809776ce24bc8b5cc5e0b9b56348a6d931fa44d
IanDataScientistIA <106856385+IanDataScientistIA@users.noreply.github.com>
2022-06-04 19:02:07 -0300

    Se depuran los comentarios
    -

--- A14G3.py
+++ A14G3.py
@@ -20,21 +20,15 @@ if ing2i(3, 4):
 #5- función módulo, retorna el módulo de 2 parámetros.
 #6- función potencia, retorna la potencia del primero elevado al segundo parámetros.
 #7- función radicación, retorna la raiz del primero respecto del segundo parámetros.
-#9- función p1, retorna el producto de los 2 primero más el 3er parámetros, usando las
-#funciones anteriores.
-#10- función p1, retorna la suma de los 2 primero por el 3er parámetros, usando las
-#funciones anteriores.
-#11- función p1, retorna la resta de los 2 primero por el 3er parámetros, usando las funciones
-#anteriores.
+#9- función p1, retorna el producto de los 2 primero más el 3er parámetros, usando las funciones anteriores.
+#10- función p1, retorna la suma de los 2 primero por el 3er parámetros, usando las funciones anteriores.
+#11- función p1, retorna la resta de los 2 primero por el 3er parámetros, usando las funciones anteriores.
 
 #NO USAR NUMPY#
 #12- función genrnd que retorna una lista con 50 números aleatorios.
-#13- función que devuelva la suma de las combinaciones posibles de los números generados
-#por la función genrnd tomados de a dos.
-#14- función que devuelva el producto de las combinaciones posibles de los números
-#generados por la función genrnd tomados de a dos.
-#15- función que devuelva el producto de las combinaciones posibles de los números
-#generados por la función genrnd tomados de a dos.
+#13- función que devuelva la suma de las combinaciones posibles de los números generados por la función genrnd tomados de a dos.
+#14- función que devuelva el producto de las combinaciones posibles de los números generados por la función genrnd tomados de a dos.
+#15- función que devuelva el producto de las combinaciones posibles de los números generados por la función genrnd tomados de a dos.
 #16- función que calcule la media del vector obtenido en genrnd.
 #17- función que calcule la mediana del vector obtenido en genrnd.
 #18- función que calcule el rango del vector obtenido en genrnd.
