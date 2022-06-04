f46b0e20eecf1bfb57862749d3dd48348105db67
IanDataScientistIA <106856385+IanDataScientistIA@users.noreply.github.com>
2022-06-04 18:59:02 -0300

    push n1
    Se sube el archivo base del proyecto con las funciones y las dos primeras soluciones

---
+++ A14G3.py
@@ -0,0 +1,52 @@
+from ing2i import ing2i #Encargada de recivir y retornar valor numerico
+from ing2s import ing2s #Encargada de recivir y retornar valor string
+
+#1- función suma, retorna la suma de 2 parámetros.
+def suma(numero1, numero2):
+    return(numero1 + numero2)
+
+if ing2i(3, 4):
+    suma(3,4)
+
+#2- función resta, retorna la resta de 2 parámetros.
+def resta(numero1, numero2):
+    return(numero1 - numero2)
+
+if ing2i(3, 4):
+    resta(3,4)
+
+#3- función producto, retorna el producto de 2 parámetros.
+#4- función cociente, retorna el cociente de 2 parámetros.
+#5- función módulo, retorna el módulo de 2 parámetros.
+#6- función potencia, retorna la potencia del primero elevado al segundo parámetros.
+#7- función radicación, retorna la raiz del primero respecto del segundo parámetros.
+#9- función p1, retorna el producto de los 2 primero más el 3er parámetros, usando las
+#funciones anteriores.
+#10- función p1, retorna la suma de los 2 primero por el 3er parámetros, usando las
+#funciones anteriores.
+#11- función p1, retorna la resta de los 2 primero por el 3er parámetros, usando las funciones
+#anteriores.
+
+#NO USAR NUMPY#
+#12- función genrnd que retorna una lista con 50 números aleatorios.
+#13- función que devuelva la suma de las combinaciones posibles de los números generados
+#por la función genrnd tomados de a dos.
+#14- función que devuelva el producto de las combinaciones posibles de los números
+#generados por la función genrnd tomados de a dos.
+#15- función que devuelva el producto de las combinaciones posibles de los números
+#generados por la función genrnd tomados de a dos.
+#16- función que calcule la media del vector obtenido en genrnd.
+#17- función que calcule la mediana del vector obtenido en genrnd.
+#18- función que calcule el rango del vector obtenido en genrnd.
+#19- función que calcule la varianza del vector obtenido en genrnd.
+#20- función que calcule devuelva el mínimo del vector obtenido en genrnd.
+#21- función que calcule devuelva el máximo del vector obtenido en genrnd.
+#22- función genrnd que retorna una lista con 500.000.000.000.000.000 números aleatorios.
+#23- función que calcule la media del vector obtenido en genrnd.
+#24- función que calcule la mediana del vector obtenido en genrnd.
+#25- función que calcule el rango del vector obtenido en genrnd.
+#26- función que calcule la varianza del vector obtenido en genrnd.
+#27- función que calcule devuelva el mínimo del vector obtenido en genrnd.
+#28- función que calcule devuelva el máximo del vector obtenido en genrnd.
+#29- medir el tiempo de ejecución del 16 al 21
+#30- medir el tiempo de ejecución del 22 al 28
