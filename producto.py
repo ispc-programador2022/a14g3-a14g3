89964c647a4dc8df86d9b332a9691ba56231c760
Fabricio Matías Toso <88169799+FaMatias@users.noreply.github.com>
2022-06-12 04:10:38 -0300

    Add files via upload
    Se sube función producto, retorna el producto de 2 parámetros.

---
+++ producto.py
@@ -0,0 +1,7 @@
+def producto(a, b):
+    return (a * b)
+
+a = int(input('Ingrese primer numero: '))
+b = int(input('Ingrese segundo numero: '))
+
+print(f'El producto de {a} y {b} es {producto(a, b)}')
