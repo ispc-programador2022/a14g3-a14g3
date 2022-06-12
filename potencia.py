ed5205e7e0baafb7c082ccb5342d1cb62553b169
Fabricio Matías Toso <88169799+FaMatias@users.noreply.github.com>
2022-06-12 04:16:00 -0300

    Create potencia.py
    Se sube función potencia, retorna la potencia del primero elevado al segundo parámetros.

---
+++ potencia.py
@@ -0,0 +1,7 @@
+def  potencia(a, b):
+    return (a ** b)
+
+a = int(input('Ingrese primer numero: '))
+b = int(input('Ingrese valor a elevar primer numero: '))
+
+print(f'La  potencia de {a} elevado al {b} es { potencia(a, b)}')
