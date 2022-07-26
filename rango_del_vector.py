a013c706f6a60b8cf126a6ea90767b92f645a439
Fabricio Matías Toso <88169799+FaMatias@users.noreply.github.com>
2022-07-26 14:43:56 -0300

    Se crea rango_del_vector

---
+++ rango_del_vector.py
@@ -0,0 +1,6 @@
+from genrnd2 import genrnd2 #Se importa desde genrnd2
+
+#Función que calcule la mediana del vector obtenido en genrnd2.
+def calcula_rango(lista):
+    res=[min(lista),max(lista)]
+    return res
