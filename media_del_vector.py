509815854247da03bad2bbdf750dc28b47087dee
Fabricio Matías Toso <88169799+FaMatias@users.noreply.github.com>
2022-07-26 14:40:51 -0300

    Se crea media_del_vector

---
+++ media_del_vector.py
@@ -0,0 +1,6 @@
+from genrnd2 import genrnd2 #Se importa desde genrnd2
+
+#Se procede a calcular la media del vector.
+def media_del_vector (lista):
+    res=cociente(suma_lista(lista),len(lista))
+    return res
