787ef8285d58ad9c670f8c05b2bca37eaaf14c26
Fabricio Matías Toso <88169799+FaMatias@users.noreply.github.com>
2022-07-26 14:53:11 -0300

    Se crea mediana_del_vector.py

---
+++ mediana_del_vector.py
@@ -0,0 +1,6 @@
+from genrnd2 import genrnd2 #Se importa desde genrnd2
+
+#Función que calcule la mediana del vector obtenido en genrnd2.
+def calcula_mediana(lista):
+    rest=cociente(suma_lista(lista),2)
+    return rest
